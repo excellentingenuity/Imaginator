11c3f71267169fc2dab6e7e8c79ab22f7fe28067
James Johnson <james.johnson@excellentingenuity.com>
2015-10-04 14:35:55 -0400

    first class and test

---
+++ src/Imaginator/Imaginator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace eig\imaginator;
+
+/**
+ * User: James Johnson
+ * Date: 10/4/15
+ * Time: 2:34 PM
+ */
+class Imaginator
+{
+
+}
---
+++ tests/imaginatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace eig\imaginator\tests;
+
+/**
+ * User: James Johnson
+ * Date: 10/4/15
+ * Time: 2:35 PM
+ */
+class imaginatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
